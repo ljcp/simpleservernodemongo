b6c8a59925d5de15dcf51bcdf02c7a5bef5c79ec
Luis Castillo <ljcp28ljcp@gmail.com>
2015-07-01 22:20:18 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+//Lets require/import the HTTP module
+var http = require('http');
+var MongoClient = require('mongodb').MongoClient
+
+
+
+// Connection URL
+var url = 'mongodb://localhost:27017/videossite';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+
+    console.log("Connected correctly to server");
+
+    //Lets define a port we want to listen to
+    const PORT=8080;
+
+    //We need a function which handles requests and send response
+    function handleRequest(request, response){
+      response.end('It Works!! ' + "Mongo Connected Url " + url  );
+    }
+
+    //Create a server
+    var server = http.createServer(handleRequest);
+
+    //Lets start our server
+    server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+    });
+});
